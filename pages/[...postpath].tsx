3d76b0a0844aece668c681733c9224d55fa1608f
funtv121 <149967727+funtv121@users.noreply.github.com>
2023-11-06 07:28:31 -0400

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://thehealthnfitness.com/`
+					` https://thehealthnfitness.com/aquaman-and-the-lost-kingdom/`
 				}`,
 			},
 		};
